890e12c579fa4744755b4a2106c32490a0c61a16
wtimme <wtimme@users.noreply.github.com>
2020-11-26 14:11:28 +0100

    Add an example view model

---
+++ GitBisectExampleApp/ExampleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExampleViewModel.swift
+//  GitBisectExampleApp
+//
+//  Created by Wolfgang Timme on 26.11.20.
+//
+
+import Foundation
+
+struct ExampleViewModel {
+    let title: String
+}
