eba572c5bc08d859d9678239845781b2f12eb99d
wtimme <wtimme@users.noreply.github.com>
2020-11-26 14:20:31 +0100

    Add delegate to the view model

--- GitBisectExampleApp/ExampleViewModel.swift
+++ GitBisectExampleApp/ExampleViewModel.swift
@@ -6,9 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+protocol ExampleViewModelDelegate: AnyObject {
+    func changeTextLabelFont(to font: UIFont)
+}
 
 struct ExampleViewModel {
     let title: String
+    
+    weak var delegate: ExampleViewModelDelegate?
     
     init(title: String = "My example app") {
         self.title = title
